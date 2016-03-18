82e5257b01c45f14a8e6bb34b662c8acdcb8952c
samnags <sam.nagourney@gmail.com>
2016-03-18 12:43:05 -0400

    exclude secrets

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+TimesWire::Base.api_key = Rails.application.secrets.nyt_key
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
